fe411c8a75450e73047df7124d6a0016e138fe6d
Michael James <mikeyj777@gmail.com>
2022-08-03 13:10:15 -0400

    lessons - 148

--- beginner_js/scripts/index.js
+++ beginner_js/scripts/index.js
@@ -12,3 +12,14 @@ list[0].querySelector("a").style.color = "yellow";
 list[1].style.color = "blue";
 
 list[2].style.color = "red";
+
+var btn = document.getElementById("btn1");
+btn.style.backgroundColor = "red";
+
+// document.querySelector("#btn1").classList.add("invisible");
+
+// document.querySelector("#btn1").classList.remove("invisible");
+
+// document.querySelector("#btn1").classList.toggle("invisible");
+
+document.querySelector("h1").classList.toggle("huge");
